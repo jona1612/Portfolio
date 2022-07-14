05ebd5aae098274828314dcfce7e694154aa1ef6
Jonathan <jonathan16official@gmail.com>
2022-07-14 13:29:36 +0530

    New

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -69,7 +69,7 @@ function Header(props) {
           <NavLink to={link} tag={Link} className="logo">
             <span style={{ color: theme.text }}></span>
             <span className="logo-name" style={{ color: theme.text }}>
-              {greeting.logo_name}
+              {greeting.logo_name} 
             </span>
             <span style={{ color: theme.text }}></span>
           </NavLink>
